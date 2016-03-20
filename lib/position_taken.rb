588f602297a411fbd8e063d66bb6506638e1d30e
AliZaman <ahmed_ali_z92@yahoo.com>
2016-03-20 20:13:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,11 @@
 def position_taken? (board, position)
-  if board[position] == " " 
+  if board[position] == " "
     false
+    elsif board[position] == ""
+    false
+    elsif board [position] == nil
+    false
+    else board[position] == "X" || board [position] == "O"
+    true
   end
 end
